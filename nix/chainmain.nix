a92ec5dc1619f837b4bff08dd6f0ca6f64a36034
jonghwan <jonghwan@crypto.com>
2021-10-05 18:36:18 +0900

    change chainmain version to current

--- nix/chainmain.nix
+++ nix/chainmain.nix
@@ -1,6 +1,6 @@
 {pkgs ? import ./default.nix {} }:
 let
-  released = (import (builtins.fetchTarball "https://github.com/crypto-org-chain/chain-main/archive/v3.1.1.tar.gz") { }).chain-maind;
+  released = (import (builtins.fetchTarball "https://github.com/crypto-org-chain/chain-main/archive/v2.1.1.tar.gz") { }).chain-maind;
 in
  pkgs.symlinkJoin {
     name = "chainmain";
