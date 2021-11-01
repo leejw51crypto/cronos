5692a85f9a83d6a6e0e8f249840ee83ce9bb2a45
jonghwan <jonghwan@crypto.com>
2021-11-01 20:36:10 +0900

    OK

--- my/strawberry/send.sh
+++ my/strawberry/send.sh
@@ -3,6 +3,7 @@ export FROM=$S1
 export TO=$S2
 
 
-export AMOUNT=1$DENOM
+#export AMOUNT=1$DENOM
+export AMOUNT=100000000234500000000000000$DENOM
 echo "send amount $AMOUNT"
 $CLI tx bank  send $FROM $TO $AMOUNT --chain-id $CHAINID --keyring-backend $KEYRING  --fees 20$DENOM --home $CHAINHOME --node $NODE
